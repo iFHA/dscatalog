c3570c0a039198d52615d6c8e903a6e2052ccb17
iFHA <fernando.everywhere@gmail.com>
2024-06-25 19:21:18 -0300

    h2 database

--- backend/src/main/java/dev/fernando/dscatalog/services/CategoryService.java
+++ backend/src/main/java/dev/fernando/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package dev.fernando.dscatalog.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.fernando.dscatalog.dto.CategoryDTO;
 import dev.fernando.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,8 @@ public class CategoryService {
     public CategoryService(final CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
+
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         return this.categoryRepository.findAll().stream().map(CategoryDTO::new).toList();
     }
