8a47e0f4469707e7efc7c6faed74bf5a63d7f4db
iFHA <fernando.everywhere@gmail.com>
2024-06-26 18:23:49 -0300

    auditing data

--- backend/src/main/java/dev/fernando/dscatalog/entities/Category.java
+++ backend/src/main/java/dev/fernando/dscatalog/entities/Category.java
@@ -1,10 +1,15 @@
 package dev.fernando.dscatalog.entities;
 
+import java.time.Instant;
+
 import dev.fernando.dscatalog.dto.CategoryDTO;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +20,11 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Category(){}
 
@@ -42,6 +52,24 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = Instant.now();
     }
 
     @Override
