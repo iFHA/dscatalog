70d0ee44d2ffbf48bc4ce42f2a7f11f7b13aa1a0
iFHA <fernando.everywhere@gmail.com>
2024-06-26 14:43:28 -0300

    PUT category

--- backend/src/main/java/dev/fernando/dscatalog/services/CategoryService.java
+++ backend/src/main/java/dev/fernando/dscatalog/services/CategoryService.java
@@ -22,13 +22,22 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         return this.categoryRepository.findAll().stream().map(CategoryDTO::new).toList();
     }
+    protected Category findEntityById(Long id) {
+        return this.categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Categoria de Id = %d não encontrada!".formatted(id)));
+    }
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
-        return new CategoryDTO(this.categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Categoria de Id = %d não encontrada!".formatted(id))));
+        return new CategoryDTO(this.findEntityById(id));
     }
     @Transactional
     public CategoryDTO store(CategoryDTO dto) {
         dto.setId(null);
         return new CategoryDTO(this.categoryRepository.save(new Category(dto)));
+    }
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        Category entity = this.findEntityById(id);
+        entity.setName(dto.getName());
+        return new CategoryDTO(this.categoryRepository.save(entity));
     }
 }
