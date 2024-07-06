6c71b569e86847a17165f9f4bfe43b88ff733398
iFHA <fernando.everywhere@gmail.com>
2024-07-06 19:40:55 -0300

    test findallpaged

--- backend/src/test/java/dev/fernando/dscatalog/services/ProductServiceIntegrationTest.java
+++ backend/src/test/java/dev/fernando/dscatalog/services/ProductServiceIntegrationTest.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.fernando.dscatalog.repositories.ProductRepository;
 import dev.fernando.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIntegrationTest {
 
     @Autowired
@@ -37,5 +41,33 @@ public class ProductServiceIntegrationTest {
     @Test
     void deleteShouldThrowResourceNotFoundWhenIdDoesNotExists() {
         Assertions.assertThrows(ResourceNotFoundException.class, ()->service.delete(nonExistingId));
+    }
+
+    @Test
+    void findAllPagedShouldReturnPageWhenPage0Size10() {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        var page = service.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(page.isEmpty());
+        Assertions.assertEquals(0, page.getNumber());
+        Assertions.assertEquals(10, page.getSize());
+        Assertions.assertEquals(countTotalProducts, page.getTotalElements());
+    }
+    @Test
+    void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExists() {
+        PageRequest pageRequest = PageRequest.of(1, 25);
+        var page = service.findAllPaged(pageRequest);
+
+        Assertions.assertTrue(page.isEmpty());
+    }
+    @Test
+    void findAllPagedShouldReturnSortedPageWhenSortByName() {
+        PageRequest pageRequest = PageRequest.of(0,10, Sort.by("name"));
+        var page = service.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(page.isEmpty());
+        Assertions.assertEquals("Macbook Pro", page.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", page.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", page.getContent().get(2).getName());
     }
 }
